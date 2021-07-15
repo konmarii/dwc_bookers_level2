c64a8d01b895ac5f5218653e2e3f78a059a37d78
konmarii <chankonworld@gmail.com>
2021-07-15 02:26:30 +0000

    [update]bookのindexページの左側の作成(User info)

--- bookers2/config/initializers/application_controller_renderer.rb
+++ bookers2/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '165d619532c13adccf5faaddde6a7d16b0fb8e6de78fd869160b66a95293784690eac4f8eea5f2f2f622fe2afd118ad1a88bfb78ae1fd0f641cc3118e672077d'
