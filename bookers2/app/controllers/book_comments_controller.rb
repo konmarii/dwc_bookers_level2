af5350a57dabc00beaa1c99e25de90a2ed8b3137
konmarii <chankonworld@gmail.com>
2021-07-25 12:51:08 +0000

    [finish]応用課題３バリデーション部分の修正

--- bookers2/app/controllers/book_comments_controller.rb
+++ bookers2/app/controllers/book_comments_controller.rb
@@ -4,8 +4,12 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    if comment.save
+      redirect_to book_path(book)
+      flash[:notice_create_book]="You have created comment successfully."
+    else
+      redirect_to book_path(book)
+    end
   end
 
   def destroy
