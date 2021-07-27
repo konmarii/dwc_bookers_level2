9e377aaca759443bf8e21e7b5ce2c4d90b270d32
konmarii <chankonworld@gmail.com>
2021-07-27 14:56:29 +0000

    [finish]応用課題５

--- bookers2/app/models/book.rb
+++ bookers2/app/models/book.rb
@@ -6,7 +6,21 @@ class Book < ApplicationRecord
     def favorited_by?(user)
       favorites.where(user_id: user.id).exists?
     end
-    
+
+    def self.search(search, word)
+      if search == "forward_match"
+        @books = Book.where("title LIKE?","#{word}%")
+      elsif search == "backward_match"
+        @books = Book.where("title LIKE?","%#{word}")
+      elsif search == "perfect_match"
+        @boosk = Book.where(title: word)
+      elsif search == "partial_match"
+        @books = Book.where("title LIKE?","%#{word}%")
+      else
+        @books = Book.all
+      end
+    end
+
     validates :title, presence: true
     validates :body, presence: true, length: { maximum: 200 }
 end
