1a03924745e0b0d2d488e2e9448a792e493cf229
konmarii <chankonworld@gmail.com>
2021-07-27 09:06:16 +0000

    [finish]応用課題４

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -8,20 +8,15 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
-  # foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定します。
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  # @user.booksのように、@user.yyyで、
-  # そのユーザがフォローしている人orフォローされている人の一覧を出したい
   has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
   has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
   
-  # ユーザーをフォローする
   def follow(user_id)
     follower.create(followed_id: user_id)
   end
 
-  # ユーザーのフォローを外す
   def unfollow(user_id)
     follower.find_by(followed_id: user_id).destroy
   end
