8a4bcabb3e211c645c5223319df1fd889c4b634c
konmarii <chankonworld@gmail.com>
2021-08-07 11:43:25 +0000

    [finish]応用課題８c

--- bookers2/app/controllers/groups_controller.rb
+++ bookers2/app/controllers/groups_controller.rb
@@ -4,17 +4,18 @@ class GroupsController < ApplicationController
         @user = current_user
         @book = Book.new
     end
-    
+
     def show
         @user = current_user
         @newbook = Book.new
         @group = Group.find(params[:id])
+        @owner = User.find(@group.owner_id)
     end
-    
+
     def new
         @group = Group.new
     end
-    
+
     def create
         @group = Group.new(group_params)
         @group.owner_id = current_user.id
@@ -25,14 +26,14 @@ class GroupsController < ApplicationController
             render :new
         end
     end
-    
+
     def edit
         @group = Group.find(params[:id])
         if @group.owner_id != current_user.id
             redirect_to groups_path
         end
     end
-    
+
     def update
         @group = Group.find(params[:id])
         @group.owner_id = current_user.id
@@ -44,21 +45,21 @@ class GroupsController < ApplicationController
             @group = Group.find(params[:id])
         end
     end
-    
+
     def join
         @group = Group.find(params[:id])
         @group.users << current_user
         @group.save
         redirect_to groups_path
     end
-    
+
      def leave
         @group = Group.find(params[:id])
         @group.users.include?(current_user)
         @group.users.delete(current_user)
         redirect_to groups_path
     end
-    
+
     private
     def group_params
         params.require(:group).permit(:name, :introduction, :image)
